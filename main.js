606608f6782c458388a938da2dfcc2f790f09cf9
ranjitm10 <ranjitmohite00@gmail.com>
2020-12-09 11:21:44 +0530

    changes

---
+++ main.js
@@ -0,0 +1,36 @@
+var btn = document.querySelector("#btn");
+var txtIn = document.querySelector("#txtInput");
+var output = document.querySelector("#divOut");
+
+var serverURL = "https://api.funtranslations.com/translate/dothraki.json";
+
+function translateURL(text){
+	return serverURL + "?" + "text=" + text; 
+}
+
+function errorHandler(error) {
+    console.log("error occured", error);
+    alert("something wrong with server! try again after some time")
+}
+
+
+// function btnHandler() {
+//     var inputText = txtInput.value; // taking input
+
+//     // calling server for processing
+//     fetch(getTranslationURL(inputText))
+//         .then(response => response.json())
+//         .then(json => {
+//             var translatedText = json.contents.translated;
+//             divOut.innerText = translatedText; // output
+//            })
+//         .catch(errorHandler)
+// };
+
+// btnTranslate.addEventListener("click", btnHandler)
+
+btn.addEventListener("click",function btnHandler(){
+	fetch(translateURL(txtIn.value)).then(response => response.json()).then(json=>{
+		divOut.innerText=json.contents.translated;
+	}).catch(errorHandler);
+});
